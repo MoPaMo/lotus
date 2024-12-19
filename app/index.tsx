cefebe55c8deaa1d4022b55a968d6eaf1c0c51d9
MoPaMo <mopamo@users.noreply.github.com>
2024-12-19 12:43:37 +0100

    Refactor app/index.tsx to integrate theme management and improve styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+// app/index.tsx
+import React from "react";
+import { AppearanceProvider } from "react-native-appearance";
+import { ThemeProvider } from "@/components/theme/ThemeContext";
+import ThemeWrapper from "@/components/theme/ThemeWrapper";
+import { View, Text } from "react-native";
+import styled from "styled-components/native";
 
-export default function Index() {
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.background};
+`;
+
+const ThemedText = styled.Text`
+  color: ${(props) => props.theme.text};
+`;
+
+const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <AppearanceProvider>
+      <ThemeProvider>
+        <ThemeWrapper>
+          <Container>
+            <ThemedText>Hello, Catppuccin!</ThemedText>
+          </Container>
+        </ThemeWrapper>
+      </ThemeProvider>
+    </AppearanceProvider>
   );
-}
+};
+
+export default App;
