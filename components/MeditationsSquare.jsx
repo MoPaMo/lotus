8f50382b75dc857a1bad7aa7153fced203f2efb3
MoPaMo <mopamo@users.noreply.github.com>
2024-12-21 15:48:29 +0100

    Refactor MeditationsSquare component to simplify styling and update structure

--- components/MeditationsSquare.jsx
+++ components/MeditationsSquare.jsx
@@ -1,57 +1,21 @@
 import React from "react";
-import { View } from "react-native";
 import styled from "styled-components/native";
 import { MaterialIcons } from "@expo/vector-icons";
 
-const Container = styled.View`
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-around;
-`;
-
-const Square = styled.View`
-  width: 48%;
-  height: 100px;
-  margin-bottom: 10px;
-  border-radius: 10px;
+const SquareContainer = styled.View`
+  flex: 1;
+  aspect-ratio: 1;
   justify-content: center;
   align-items: center;
-  elevation: 5;
-  shadow-color: #000;
-  shadow-opacity: 0.25;
-  shadow-radius: 3.84px;
-  shadow-offset: 0px 2px;
+  background-color: ${(props) => props.theme.rosewater};
 `;
 
-const MeditationSquare = styled(Square)`
-  background-color: ${(props) => props.theme.mauve};
-`;
-
-const OrangeSquare = styled(Square)`
-  background-color: ${(props) => props.theme.peach};
-`;
-
-const PurpleSquare = styled(Square)`
-  background-color: ${(props) => props.theme.lavender};
-`;
-
-const GreenSquare = styled(Square)`
-  background-color: ${(props) => props.theme.green};
-`;
-
-const MeditationText = styled.Text`
-  font-family: "Poppins";
-  font-size: 16px;
-  color: ${(props) => props.theme.text};
-`;
-
-const SquareView = () => {
+const SquareWithSymbol = () => {
   return (
-    <MeditationSquare>
-      <MaterialIcons name="meditation" size={40} color="white" />
-      <MeditationText>Meditation</MeditationText>
-    </MeditationSquare>
+    <SquareContainer>
+      <MaterialIcons name="home" size={48} color="#FFFFFF" />
+    </SquareContainer>
   );
 };
 
-export default SquareView;
+export default SquareWithSymbol;
