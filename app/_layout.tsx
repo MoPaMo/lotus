b393119b20fc23205f38454103540d183d136370
MoPaMo <mopamo@users.noreply.github.com>
2024-12-23 23:28:33 +0100

    Refactor Layout component for consistent formatting and add screenOptions to Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,20 +7,20 @@ import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
 import AppLoading from "expo-app-loading";
 
 const Layout = () => {
-  let [fontsLoaded] = useFonts({
-    Poppins_400Regular,
-  });
+    let [fontsLoaded] = useFonts({
+        Poppins_400Regular,
+    });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-  return (
-    <ThemeProvider>
-      <ThemeWrapper>
-        <Stack />
-      </ThemeWrapper>
-    </ThemeProvider>
-  );
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+    return (
+        <ThemeProvider>
+            <ThemeWrapper>
+                <Stack screenOptions={{ headerShown: false }} />
+            </ThemeWrapper>
+        </ThemeProvider>
+    );
 };
 
 export default Layout;
