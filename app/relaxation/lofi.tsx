d25a627f220981d3df42b6c7336311c9cb1f8ed0
MoPaMo <mopamo@users.noreply.github.com>
2024-12-23 22:34:05 +0100

    Add Lofi component to render LofiPlayer within a styled view

---
+++ app/relaxation/lofi.tsx
@@ -0,0 +1,11 @@
+import LofiPlayer from "@/components/LoFiPlayer";
+import styled from "styled-components/native";
+import { View } from "react-native";
+
+export default function Lofi() {
+  return (
+    <View>
+      <LofiPlayer />
+    </View>
+  );
+}
